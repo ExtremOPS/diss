9ab6401fd1fe91b683dbf91c48e005afa5ee0c7a
Christoph Goering <goering@imes.mavt.ethz.ch>
2021-11-19 14:27:26 +0100

    script to append to labels something

---
+++ SECTION/change_labels.py
@@ -0,0 +1,31 @@
+import re
+import os
+import glob
+root = os.path.dirname(os.path.abspath(__file__))
+folder = '30_Timeconstant'
+prefix = 'TC'
+
+
+class File(object):
+    def __init__(self, file_name, method):
+        self.file_obj = open(file_name, method)
+
+    def __enter__(self):
+        return self.file_obj
+
+    def __exit__(self, type, value, traceback):
+        self.file_obj.close()
+
+
+pattern = r'(sec|tab|eq):(.+)}'
+
+
+search_folder = os.path.join(root, folder, '20_Sections/')
+names = glob.glob(search_folder+'*.tex')
+for name in names:
+    with open(name, "r") as myfile:
+        data = myfile.readlines()
+    with open(name, "wt") as myfile:
+        for line in data:
+            #  print(line)
+            myfile.write(re.sub(pattern, r'\1:TC-\2}', line))
