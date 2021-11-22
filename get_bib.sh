cb670074b907c3e9581f3148914709e3c92f5d25
Christoph Goering <goering@imes.mavt.ethz.ch>
2021-11-22 11:14:30 +0100

    converting month name to number

--- get_bib.sh
+++ get_bib.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 cp /Users/goeringc/Documents/Mendeley/00_BIB/All.bib /Users/goeringc/Documents/15_Git/diss/
 
+
+
+sed -i '' 's/{jan}/{1}/g' /Users/goeringc/Documents/15_Git/diss/All.bib
+sed -i '' 's/{feb}/{2}/g' /Users/goeringc/Documents/15_Git/diss/All.bib
+sed -i '' 's/{mar}/{3}/g' /Users/goeringc/Documents/15_Git/diss/All.bib
+sed -i '' 's/{may}/{4}/g' /Users/goeringc/Documents/15_Git/diss/All.bib
+sed -i '' 's/{apr}/{5}/g' /Users/goeringc/Documents/15_Git/diss/All.bib
+sed -i '' 's/{jun}/{6}/g' /Users/goeringc/Documents/15_Git/diss/All.bib
+sed -i '' 's/{jul}/{7}/g' /Users/goeringc/Documents/15_Git/diss/All.bib
+sed -i '' 's/{aug}/{8}/g' /Users/goeringc/Documents/15_Git/diss/All.bib
+sed -i '' 's/{sep}/{9}/g' /Users/goeringc/Documents/15_Git/diss/All.bib
+sed -i '' 's/{oct}/{10}/g' /Users/goeringc/Documents/15_Git/diss/All.bib
+sed -i '' 's/{nov}/{11}/g' /Users/goeringc/Documents/15_Git/diss/All.bib
+sed -i '' 's/{dec}/{12}/g' /Users/goeringc/Documents/15_Git/diss/All.bib
